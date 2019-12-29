e632effb0708a29d14b59fdd456ae7bf44e60916
cooltaby <cooltaby@users.noreply.github.com>
2019-12-29 19:47:41 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1577628919,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/wall.png",
+		"images/goalred.png",
+		"images/padlered-sheet0.png",
+		"images/padlered-sheet1.png",
+		"images/puck-sheet0.png",
+		"images/table-sheet0.png",
+		"images/airhockeylocalmultiplayer-sheet0.png",
+		"images/goalblue.png",
+		"images/paddleblueposition-sheet0.png",
+		"images/airhockey-sheet0.png",
+		"images/spritelocalmultiplayer-sheet0.png",
+		"images/spritenetworkmultiplayer-sheet0.png",
+		"images/spritesingleplayer-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
